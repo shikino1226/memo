a7859f1c5421b01bfed56aec5b595757b53eda19
shikino <sshhiikkiinnoo@gmail.com>
2021-09-18 15:33:19 +0900

    メモアプリ

---
+++ memo.rb
@@ -0,0 +1,38 @@
+require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+
+memo_number = gets.to_i
+
+if memo_number == 1
+
+ puts "新規でメモを作成します。"
+ puts "拡張子を除いたファイル名を入力してください"
+
+ file_name = gets.chomp
+ puts "「#{file_name}.csv」という名前のファイルを作成しました。"
+ puts "メモしたい内容を記入してください"
+ p "完了したら Ctrl+D　を入力します"
+ 
+ memo_type = readlines
+ CSV.open("#{file_name}.csv", 'w') do |csv|
+ csv << ["#{memo_type}"]
+ end
+
+
+elsif memo_number == 2
+  puts "編集するファイル名を拡張子を除いて入力してください"
+  file_name = gets.chomp
+
+ puts "(#{file_name}.csv)に追記します"
+ puts "追記したい内容を入力してください"
+ p "完了したら Ctrl+D　を入力します"
+
+ memo_type = readlines
+ CSV.open("#{file_name}.csv", 'a') do |csv|
+ csv << ["#{memo_type}"]
+ end
+
+else
+ puts "エラー。1か2を選択してください"
+end
